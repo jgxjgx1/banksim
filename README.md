91a0dd1003ae270a14de0b2189bf726915023bb2
jgxjgx1 <155945078+jgxjgx1@users.noreply.github.com>
2024-04-26 12:26:12 +0300

    banksim

---
+++ README.md
@@ -0,0 +1,8 @@
+## ATM practice project
+A simulated ATM utilizing a MySQL database and REST API. Withdrawing other currencies is simulated as well.
+
+The currency rates are fetched daily online.
+____
+This was developed for an RFID card reader (Olimex MOD-RFID125) but the card number can be entered without it if the card reader related code is changed.
+
+`env_example file (inside backend folder) needs to be renamed .env and the contents of it need to be edited accordingly before use.`
---
+++ bank-automat/cardtypeui.cpp
@@ -0,0 +1,31 @@
+#include "cardtypeui.h"
+#include "ui_cardtypeui.h"
+
+cardtypeui::cardtypeui(QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::cardtypeui)
+{
+    ui->setupUi(this);
+    //this->showFullScreen();
+}
+
+cardtypeui::~cardtypeui()
+{
+    qDebug()<<"cardtype poistettu";
+    delete ui;
+}
+
+
+void cardtypeui::on_btn_credit_clicked()
+{
+    soundObj.playSound(4);
+    url_idcard_combi = "http://localhost:3000/card_privileges/getAccountCredit/"+id_card;
+    this->close();
+}
+
+void cardtypeui::on_btn_debit_clicked()
+{
+    soundObj.playSound(4);
+    url_idcard_combi = "http://localhost:3000/card_privileges/getAccountDebit/"+id_card;
+    this->close();
+}
